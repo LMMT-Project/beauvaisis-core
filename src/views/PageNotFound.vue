44ebf22bf754c4ea636f2d55354f82797a5d27be
tosu! <59777055+tosu-dev@users.noreply.github.com>
2022-10-11 22:58:20 +0200

    Add hover effect to nav items, and style error 404 page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1>Error 404</h1>
-  <h1>Page not found</h1>
+  <div class="pagenotfound-container">
+    <h1>Error 404</h1>
+    <h1>Page not found</h1>
+    <a href="/">Return to website</a>
+  </div>
 </template>
 
 <style scoped>
-  h1 {
-    font-family: Arial;
-    text-align: center;
-  }
+.pagenotfound-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+h1 {
+  text-align: center;
+}
+
+a {
+  display: inline-block;
+  width: 100%;
+  text-align: center;
+}
 </style>
