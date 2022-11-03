6aa9276d4c7aa2f3f6ce9806859b3c3a080f63d7
tosu! <59777055+tosu-dev@users.noreply.github.com>
2022-11-03 21:49:23 +0100

    change roots

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Articles from "../views/Articles.vue";
+import Articles from "../views/Article.vue";
 import PageNotFound from "../views/PageNotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/Home",
+      path: "/",
       name: "Home",
       component: Home,
     },
     {
-      path: "/Articles",
-      name: "Articles",
+      path: "/Article",
+      name: "Article",
       component: Articles,
     },
     {
